48db162bead618f6d5e5012c9d70ac4eade2efff
njabulomlang <50872115+njabulomlang@users.noreply.github.com>
2020-10-20 03:17:29 -0700

    changes

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as HighCharts from 'highcharts';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +9,111 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  price = 3;
+  recycleType: { totalMass: number, name: string, bgcolor: string }[] = [
+    { "totalMass": 0, "name": "Outbound", "bgcolor":"#C5A60A" },
+    { "totalMass": 0, "name": "Inbound", "bgcolor":"#D12C6F" },
+    { "totalMass": 0, "name": "Reclaimer", "bgcolor":"#13BBBB" }
+];
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
+  ionViewDidEnter() {
+    this.plotSimpleBarChart();
+    this.plotSimpleBarChart1();
+    this.plotSimpleBarChart2();
+  }
 
+  plotSimpleBarChart() {
+    let myChart = HighCharts.chart('highcharts', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Inbound'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [
+        {
+          name: 'Jane',
+          type: undefined,
+          data: [1, 0, 4]
+        },
+        {
+          name: 'John',
+          type: undefined,
+          data: [5, 7, 3]
+        }]
+    });
+  }
+
+  plotSimpleBarChart1() {
+    let myChart = HighCharts.chart('highcharts1', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Outbound'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [
+        {
+          name: 'Jane',
+          type: undefined,
+          data: [1, 0, 4]
+        },
+        {
+          name: 'John',
+          type: undefined,
+          data: [5, 7, 3]
+        }]
+    });
+  }
+
+  plotSimpleBarChart2() {
+    let myChart = HighCharts.chart('highcharts2', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Reclaimer'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [
+        {
+          name: 'Jane',
+          type: undefined,
+          data: [1, 0, 4]
+        },
+        {
+          name: 'John',
+          type: undefined,
+          data: [5, 7, 3]
+        }]
+    });
+  }
 }
